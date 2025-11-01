2fe3eeb9e26d5e2ba71a7be41458fb74d0f14650
Ramin Leon Neymeyer <ramin.leon.neymeyer@gmail.com>
2025-11-01 16:49:50 +0100

    merged prices into dataset

--- create_dataset.py
+++ create_dataset.py
@@ -1,19 +1,23 @@
-import pandas as pd
+from datetime import timedelta
 import matplotlib.pyplot as plt
-import requests
 from meteostat import Hourly
+import pandas as pd
+import requests
 
 def load_power_plant_data(path: str = 'data/power_plant.csv') -> pd.DataFrame:
     df_power_plant = pd.read_csv(path, delimiter=';')
-    df_power_plant = df_power_plant[df_power_plant['Power'] != '-']
-    df_power_plant['Power'] = df_power_plant['Power'].str.replace(',', '.').astype(float)
-    df_power_plant['Datetime'] = pd.to_datetime(df_power_plant['Datetime'], format='%d.%m.%Y %H:%M')
-    df_power_plant = df_power_plant.reindex(columns=['Datetime', 'Power'])
+    df_power_plant['Datetime'] = pd.to_datetime(df_power_plant['Datetime'], format='%Y-%m-%d %H:%M:00')
+    df_power_plant['Datetime'] = (df_power_plant['Datetime']
+                                    .dt.tz_localize('Europe/Berlin', ambiguous='infer', nonexistent='shift_forward')
+                                    .dt.tz_convert('UTC'))
     return df_power_plant
 
 def load_market_data(path: str = 'data/day_ahead_prices.csv') -> pd.DataFrame:
     df_market = pd.read_csv(path, delimiter=';')
     df_market['Datetime'] = pd.to_datetime(df_market['Datetime'], format='%Y-%m-%d %H:%M:00')
+    df_market['Datetime'] = (df_market['Datetime']
+                                .dt.tz_localize('Europe/Berlin', ambiguous='infer', nonexistent='shift_forward')
+                                .dt.tz_convert('UTC'))
     return df_market
 
 def fetch_holiday_data(years: list[int], region: str = 'de-be') -> pd.DataFrame:
@@ -28,8 +32,8 @@ def fetch_holiday_data(years: list[int], region: str = 'de-be') -> pd.DataFrame:
     return df_holidays
 
 def fetch_weather_data(start: pd.Timestamp, end: pd.Timestamp, station_id: str) -> pd.DataFrame:
-    weather = Hourly(station_id, start, end)
-    df_weather = weather.fetch()
+    df_weather = Hourly(station_id, start, end).fetch()
+    df_weather.index = df_weather.index.tz_localize('UTC')
     df_weather = df_weather.reset_index()
 
     df_weather = df_weather.rename(columns={
@@ -47,10 +51,13 @@ def fetch_weather_data(start: pd.Timestamp, end: pd.Timestamp, station_id: str) 
         'coco': 'Weather Condition Code'
     })
 
-    df_weather['Datetime'] = pd.to_datetime(df_weather['Datetime'])
-    df_weather = df_weather.set_index('Datetime').sort_index()
-    df_weather = df_weather.resample('15min').interpolate(method='linear')
-    df_weather = df_weather.reset_index()
+    df_weather = (df_weather
+                    .set_index('Datetime')
+                    .sort_index()
+                    .resample('15min')
+                    .interpolate(method='linear')
+                    .reset_index())
+    
     return df_weather
 
 # load power plant data
@@ -64,9 +71,10 @@ years = df['Datetime'].dt.strftime("%Y").unique()
 df_holidays = fetch_holiday_data(years=years, region='de-be')
 
 # fetch weather data
-start = df['Datetime'].min()
-end = df['Datetime'].max()
+start = df['Datetime'].min().tz_localize(None)
+end = df['Datetime'].max().tz_localize(None) + timedelta(hours=1)
 df_weather = fetch_weather_data(start=start, end=end, station_id='10582')
+df_weather.to_csv('data/weather.csv', index=False)
 
 # time-based features
 df['Holiday'] = df['Datetime'].dt.date.isin(df_holidays['Holiday'])
@@ -77,3 +85,7 @@ df['IsWeekend'] = df['DayOfWeek'].isin([5,6]).astype(int)
 
 # merge dataframes
 df = pd.merge(df, df_weather, on=['Datetime'], how='left')
+df = pd.merge(df, df_market, on=['Datetime'], how='left')
+df.to_csv('data/dataset.csv', sep=';', index=False)
+print(df.head())
+print(df.tail())
